9629107d7ebd6e4e39e95ab3f6d5d5bcddc807ce
stickfr <20114698+stickfr@users.noreply.github.com>
2022-11-11 18:29:41 -0500

    Restored BanWatch timer

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		updatePlayerData(client); 
-		//setInterval(_ => { updatePlayerData(client); }, 5 * 60 * 1000); // Every 5 minutes
+		setInterval(_ => { updatePlayerData(client); }, 5 * 60 * 1000); // Every 5 minutes
 	},
 };
