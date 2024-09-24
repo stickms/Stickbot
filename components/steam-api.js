f3bd5b08b1c14da63efead064084c8069ad7a9fd
stickms <20114698+stickms@users.noreply.github.com>
2024-09-24 13:21:42 -0400

    Added getPlayerBans() to steamapi functions and refactored steam-api code

--- components/steam-api.js
+++ components/steam-api.js
@@ -8,38 +8,72 @@ class SteamAPI {
 
   static #tokenlist = null;
   static #token = null;
-  static #curtoken = 0;
+  static #tokennum = 0;
 
   static #updateApiKey() {
     if (!this.#tokenlist) {
       this.#tokenlist = process.env.STEAM_TOKENS.split(',');
     }
 
-    this.#token = this.#tokenlist[this.#curtoken++];
+    this.#token = this.#tokenlist[this.#tokennum++];
 
-    if (this.#curtoken >= this.#tokenlist.length) 
-      this.#curtoken = 0;
+    if (this.#tokennum >= this.#tokenlist.length) 
+      this.#tokennum = 0;
   }
 
-  static async getProfileSummaries(...profiles) {
-    try {
-      this.#updateApiKey();
+  static async #callSteamApi(func, params) {
+    this.#updateApiKey();
 
-      const resp = await axios.get(this.#endpoint + 'GetPlayerSummaries/v2/', {
+    try {
+      const resp = await axios.get(this.#endpoint + func, {
         timeout: 1000,
         params: {
-          key: this.#token,
-          steamids: profiles.join(',')
+          ...params,
+          key: this.#token
         }
       });
 
-      console.log(resp.data.response.players);
+      return resp.data;
     } catch (error) {
       console.error(error);
+      return null;
     }
+  }
+
+  static async getProfileSummaries(...profiles) {
+    if (!profiles?.length) {
+      return null;
+    }
+
+    const data = await this.#callSteamApi('GetPlayerSummaries/v2/', {
+      steamids: profiles.join(',')
+    });
+
+    if (profiles.length == 1) {
+      return data?.response?.players?.[0];
+    }
+
+    return data?.response?.players;
+  }
+
+  static async getPlayerBans(...profiles) {
+    if (!profiles?.length) {
+      return null;
+    }
+
+    const data = await this.#callSteamApi('GetPlayerBans/v1/', {
+      steamids: profiles.join(',')
+    });
+
+    if (profiles.length == 1) {
+      return data?.players?.[0];
+    }
+
+    return data?.players;
   }
 }
 
-SteamAPI.getProfileSummaries('76561197960287930');
+console.log(await SteamAPI.getProfileSummaries('76561197960287930'));
+console.log(await SteamAPI.getPlayerBans('76561197960287930'));
 
 export default SteamAPI;
